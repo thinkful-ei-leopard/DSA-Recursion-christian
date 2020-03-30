1d1960749faed26468514bffa15e30bf26822fa9
shimmy77 <christianshim@gmail.com>
2020-03-30 15:41:09 -0500

    anagram

--- practice.js
+++ practice.js
@@ -75,8 +75,8 @@ function factorial(num) {
 }
 console.log(factorial(5))
 
-//find a way out of the maze
-let maze = [
+//find a way out of the maze and all the ways
+const maze = [
     [' ', ' ', ' ', '*', ' ', ' ', ' '],
     ['*', '*', ' ', '*', ' ', '*', ' '],
     [' ', ' ', ' ', ' ', ' ', ' ', ' '],
@@ -84,15 +84,84 @@ let maze = [
     [' ', ' ', ' ', ' ', ' ', ' ', 'e']
 ];
 
-function escape() {
+function escape(maze, str = '') {
+    const rs = str.split('').filter(c => c === 'R').length;
+    const ls = str.split('').filter(c => c === 'L').length;
+    const us = str.split('').filter(c => c === 'U').length;
+    const ds = str.split('').filter(c => c === 'D').length;
+    const x = rs - ls;
+    const y = ds - us;
+  
+    let up;
+    let right;
+    let down;
+    let left;
+    if (maze[y - 1]) up = maze[y - 1][x];
+    right = maze[y][x + 1];
+    if (maze[y + 1]) down = maze[y + 1][x];
+    left = maze[y][x - 1];
+  
     
-}
-
-//find all the ways out of the maze
-
+    if ( 
+      (up === undefined || up === '*') &&
+      (right === undefined || right === '*') &&
+      (down === undefined || down === '*') &&
+      (left === undefined || left === '*')
+    ) { return; }
+    
+    if (up === 'e') console.log(str + 'U');
+    if (right === 'e') console.log(str + 'R');
+    if (down === 'e') console.log(str + 'D');
+    if (left === 'e') console.log(str + 'L');
+  
+    
+    if (up && up !== '*') {
+      const newMaze = [...maze].map(line => [...line]);
+      newMaze[y][x] = '*';
+      escape(newMaze, str + 'U');
+    }
+    if (right && right !== '*') {
+      const newMaze = [...maze].map(line => [...line]);
+      newMaze[y][x] = '*';
+      escape(newMaze, str + 'R');
+    }
+    if (down && down !== '*') {
+      const newMaze = [...maze].map(line => [...line]);
+      newMaze[y][x] = '*';
+      escape(newMaze, str + 'D');
+    }
+    if (left && left !== '*') {
+      const newMaze = [...maze].map(line => [...line]);
+      newMaze[y][x] = '*';
+      escape(newMaze, str + 'L');
+    }
+  }
+  
+  escape(maze);
 
 //anagrams
-
+function anagram(str) {
+    if (str.length < 2) {
+      return str;
+    } else {
+      let shuffle = [];
+      for (let i = 0; i < str.length; i++) {
+        let char = str[i];
+        
+        if (str.indexOf(char) != i)
+          continue;
+        
+        let moreLetters = str.slice(0, i) + str.slice(i + 1, str.length);
+        
+        for (let remainderLetters of anagram(moreLetters))
+          shuffle.push(char + remainderLetters);
+      }
+      return shuffle;
+    }
+  }
+  let thisString = 'abc';
+  console.log(anagram(thisString));
+  
 
 //organization chart
 
